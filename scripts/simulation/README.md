44d7ae20dff6777c7b252ef03ee1192359e7cedc
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2026-04-29 00:41:59 +0700

    prepare script to run on dgx-a100 for mnli runs

--- scripts/simulation/README.md
+++ scripts/simulation/README.md
@@ -1,2 +1,1 @@
 Running with flower built-in simulation mode using Ray.
-Not used for testing because we cannot manage the heterogenous network configuration.
--- scripts/simulation/run.sh
+++ scripts/simulation/run.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=flowertune-distilbert-lora
+#SBATCH --job-name=UFLoRA
 #SBATCH --output=results/logs/%j/out.txt
 #SBATCH --error=results/logs/%j/err.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=64G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40G
 #SBATCH --qos=1gpu
 #SBATCH --partition=dgx-a100
 #SBATCH --gpus=1
 #SBATCH --mail-user=ilham.abdillah.alhamdi@gmail.com
 #SBATCH --mail-type=ALL
 
-DATASET=$1
-
-if [ -z "$DATASET" ]; then
-    echo "Usage: $0 <dataset>"
-    exit 1
-fi
-
 # Variables
 SIF_PATH=/srv/images/python_3.12.9.sif
-INSTANCE_NAME=flowertune-distilbert-${DATASET}      
+INSTANCE_NAME=uflora-testbed      
 PROJECT_ROOT=$(pwd)
 SETUP_ENV_SCRIPT=${PROJECT_ROOT}/scripts/setup-env.sh
 
 export SINGULARITYENV_FLWR_HOME=${PROJECT_ROOT}/.flwr
-export SINGULARITYENV_HF_HOME=${PROJECT_ROOT}/.cache/huggingface
-export SINGULARITYENV_WANDB_CACHE_DIR=${PROJECT_ROOT}/.cache/wandb
 
 # Set environment variables for Singularity to use the local Python user base
 export SINGULARITYENV_PYTHONUSERBASE=${PROJECT_ROOT}/.local
@@ -50,8 +41,11 @@ singularity exec --nv \
 
         source .venv/bin/activate && \
         flwr config list && \
-        flwr run . --stream --run-config \"dataset='$DATASET' wandb.run-name='dgx-run'\"
+        python setup.py batch --batch-config group1-300.yaml --detached\"
     "
 
-echo "Stopping Singularity instance..."
-singularity instance stop $INSTANCE_NAME
+# echo "Stopping Singularity instance..."
+# singularity instance stop $INSTANCE_NAME
+
+# Wait indefinitely until timeout
+while true; do sleep 1; done
